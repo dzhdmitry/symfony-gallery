306ec25e2261a003adbb118c09470a3259a17d3d
Dmitry <dzhdmitry@gmail.com>
2016-09-13 20:43:29 +1000

    Tests

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Gallery
 =======
 
 Gallery Symfony 3 app
+
+## Testing
+
+Run `phpunit -c app/`
+
+## License
+
+Licensed under the [MIT license](https://github.com/dzhdmitry/dx/blob/master/LICENSE).
--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -9,10 +9,60 @@ class DefaultControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertEquals(1, $crawler->filter('#viewport')->count());
+    }
+
+    public function testAlbums()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/', [], [], [
+            'HTTP_X-Requested-With' => 'XMLHttpRequest'
+        ]);
+
+        $albums = json_decode($client->getResponse()->getContent());
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertInternalType('array', $albums);
+        $this->assertCount(5, $albums);
+
+        foreach ($albums as $i => $album) {
+            $expectedCount = ($i == 0) ? 5 : 10;
+
+            $this->assertCount($expectedCount, $album->images);
+        }
+    }
+
+    public function testAlbum()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/album/2', [], [], [
+            'HTTP_X-Requested-With' => 'XMLHttpRequest'
+        ]);
+
+        $images = json_decode($client->getResponse()->getContent());
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertInternalType('array', $images->data);
+        $this->assertCount(10, $images->data);
+    }
+
+    public function testAlbumPage()
+    {
+        $client = static::createClient();
+
+        for ($i=2; $i<6; $i++) {
+            $client->request('GET', '/album/' . $i . '/page/2', [], [], [
+                'HTTP_X-Requested-With' => 'XMLHttpRequest'
+            ]);
+
+            $images = json_decode($client->getResponse()->getContent());
+
+            $this->assertEquals(200, $client->getResponse()->getStatusCode());
+            $this->assertInternalType('array', $images->data);
+            $this->assertCount(10, $images->data);
+        }
     }
 }
